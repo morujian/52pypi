dbe9c20dae285bf7028f0ed8fd2e0e62dc89cb6e
‘liusl’ <myleiwen@gmail.com>
2020-01-28 17:39:50 +0800

    xxx

--- my_pypi/settings.py
+++ my_pypi/settings.py
@@ -82,30 +82,19 @@ WSGI_APPLICATION = 'my_pypi.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            # 'ENGINE': 'django.db.backends.sqlite3',
-            # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'my_pypi',
-            'USER': 'root',
-            'PASSWORD': 'leiwen646',
-            'HOST': 'localhost',
-            'PORT': 3306,
-        }
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_pypi',
+        'USER': 'root',
+        'PASSWORD': 'leiwen646',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'my_pypi',
-            'USER': 'root',
-            'PASSWORD': 'leiwen646',
-            'HOST': '120.24.243.190',
-            'PORT': 3306,
-        }
-    }
+}
 
 
 # Password validation
